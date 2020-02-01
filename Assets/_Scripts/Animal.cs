6ef7993edd8d555316f41f15edc794ca3c665992
Curtis Cummings <curtis@parallaxvisions.com>
2020-01-31 20:04:28 -0600

    init

---
+++ Assets/_Scripts/Animal.cs
@@ -0,0 +1,9 @@
+using System;
+using UnityEngine;
+
+
+public class Animal : Monobehaviour {
+
+    public Animal myLove;
+    public Animal myPartner;
+}
---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5.0f;
+
+    [SerializeField]
+    private Animator playerAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    private void HandleMovement() {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = new Vector3(moveX, moveY, 0.0f) * moveSpeed;
+
+        transform.Translate(moveDir * Time.deltaTime);
+    }
+}
